8f2fd6012cabe2f7e4f94ae6836d4d10cdafc743
Disa Soni <113198700+DisaSoni@users.noreply.github.com>
2024-02-09 13:02:21 -0500

    Use Reduce, State, Effect

--- src/componets/Resturant.js
+++ src/componets/Resturant.js
@@ -3,14 +3,43 @@ import "./style.css"
 import Menu from './menuApi.js';
 import  MenuCard  from './MenuCard';
 
+
+const uniqueList = [
+    ...new Set(
+        Menu.map((curElem) => {
+            return curElem.category;
+        })
+    )
+]
+//Spred operator if you use many time code like curElem
+
 const Resturant = () => {
     //React Hooks useState Hook
 
     const[menuData, setMenuData] = useState(Menu);
 
+    const filterItem =(category) => {
+        const updateList =Menu.filter((curElem) =>{
+            return curElem.category === category;
+        });
+        setMenuData(updateList);
+    };
+
   return ( //menuData is prop method that fetch all data 
     <>
+    <nav className='navbar'>
+        <div className='btn-group'>
+            <button className='btn-group__item' onClick={() => filterItem("Party")}>
+            Party
+            </button>
+            <button className='btn-group__item' onClick={() => filterItem("SoftGlam")}>SoftGlam</button>
+            <button className='btn-group__item' onClick={() => filterItem("Natural")}>Natural</button>
+            <button className='btn-group__item' onClick={() => setMenuData(Menu)}>All</button>
+        </div>
+    </nav>
+
       <MenuCard menuData ={menuData}/>  
+
     </>
   );
 };
